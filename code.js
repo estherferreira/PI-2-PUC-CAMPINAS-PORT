f841ee08de3382394bce0befa0fe15dc8634b8df
NailliwFerreir <williansferreir@icloud.com>
2022-10-16 11:28:21 -0300

    Exibir codigo do bilhete

--- code.js
+++ code.js
@@ -1,9 +1,14 @@
 //Gera um número aleatório de 6 dígitos
-function getRandomNumber() {
+function createRandomNumber() {
     const randomNumber = (Math.random() * (1000000 - 100000) + 100000).toFixed(0);
-    console.log(randomNumber);
+    return randomNumber;
 }
-
+//função para exibir o codigo
+function getRandomNumber(){
+    let visor = document.getElementById('numrandomid');
+    let numberrand = createRandomNumber();
+   visor.innerHTML = 'O número do seu Bilhete é : '+ numberrand;
+}
 //Verifica se a checkbox está marcada e permite que usuário gere o código do seu bilhete caso esteja
 function toggleButton() {
     const checkbox = document.querySelector('#checkbox').checked;
@@ -11,7 +16,7 @@ function toggleButton() {
     if (checkbox) {
         document.querySelector('#generate').disabled = false;
         if (generate == false) {
-            getRandomNumber();
+           getRandomNumber();
         }
         return
     }
