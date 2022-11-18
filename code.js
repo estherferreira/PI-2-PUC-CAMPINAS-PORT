6f7155bc651d9f2adb067229065d2967b47a7691
NailliwFerreir <williansferreir@icloud.com>
2022-11-18 08:11:40 -0300

    SegundaEntrega

--- code.js
+++ code.js
@@ -1,49 +1,65 @@
+//-----------------Primeira Entrega-------------------------------
+
+
 //Gera um número aleatorio para o Bilhete
-function createRandomNumber() {
-    const randomNumber = (Math.random() * (1000000 - 100000) + 100000).toFixed(0);
-    return randomNumber;
+function createRandomNumber () {
+  const randomNumber = (Math.random() * (1000000 - 100000) + 100000).toFixed(0);
+  return randomNumber;
 }
-
 //Exibe o código do bilhete para o usuário
-function showId() {
-    let visor = document.getElementById('numrandomid');
-    let numberrand = createRandomNumber();
-    visor.innerHTML = `<button type="button" class="white-elements numberandom" name="numberandom" id="numrandomid">Seu bilhete: ${numberrand}
-    </button>`
-    return numberrand;
+function showId () {
+  let visor = document.getElementById('numrandomid');
+  let numberrand = createRandomNumber();
+  visor.innerHTML = `<button type="button" class="white-elements numberandom" name="numberandom" id="numrandomid">Seu bilhete: ${numberrand}
+  </button>`
+  return numberrand;
 }
-
 //Verifica se a checkbox está marcada e permite que usuário gere o código do seu bilhete caso esteja
-function toggleButton() {
-    const checkbox = document.querySelector('#checkbox').checked;
+function toggleButton () {
+  const checkbox = document.querySelector('#checkbox').checked;
 
-    if (checkbox) {
-        document.querySelector('#generate').disabled = false;
-        if (generate == false) {
-            showId();
-        }
-        return
+  if (checkbox) {
+    document.querySelector('#generate').disabled = false;
+    if (generate == false) {
+      showId();
     }
-    document.querySelector('#generate').disabled = true;
+    return
+  }
+  document.querySelector('#generate').disabled = true;
 }
-
+// Envia o Bilhete para o backend  
  function cadastraBilhete() {
   
   let id = showId();
 
   var xhr = new XMLHttpRequest();             
-    xhr.open("POST", "http://localhost:3000/Bilhete", true);             
-    xhr.setRequestHeader('Content-Type', 'application/json');             
-    xhr.send(JSON.stringify({                 
-        id:id,           
-    }));
+  xhr.open("POST", "http://localhost:3000/Bilhete", true);             
+  xhr.setRequestHeader('Content-Type', 'application/json');             
+  xhr.send(JSON.stringify({                 
+    id:id,           
+  }));
 }
 
-//------------------------------------------------------------------------------daqui para cima é da primeira entrega
 
-//Seleciona as modalidades de bilhetes
+//-----------------------Segunda Entrega-------------------------------
+
+
+// Limita o input a numeros e somente 6 digitos e 
+window.onload = function()
+{
+  document.getElementById('ticketCodeField').addEventListener('keyup', function(e)
+  {
+    this.value = this.value.replace(/[^0-9]/g,'');
+    let i = document.getElementById('ticketCodeField').value
+    var cond = i.length;
+    if(cond <= 6)
+    {
+      handleChange();//chama a funcao que confere se tudo foi devidamente preenchido
+    }
+  });
+};
+// Seleciona as modalidades de bilhetes
 const dropdowns = document.querySelectorAll(".dropdown");
-
 dropdowns.forEach(dropdown => {
   const select = dropdown.querySelector(".select");
   const caret = dropdown.querySelector(".caret");
@@ -70,37 +86,66 @@ dropdowns.forEach(dropdown => {
       handleChange();
     });
   });
-  
 });
-
-//Ativa botão "Comprar" na seção recarga e mostra a mensagem "Compra efetuada!"
-function showMessage() {
+//Mensagem "Compra efetuada!"
+function showMessageSuccess() {
   let message = document.getElementById('successMessage');
   message.innerHTML = `<p id="successMessage" class="success">Compra efetuada!</p>`
 }
-
-function handleChange() {
-  const ticketCode = document.querySelector('#ticketCodeField').value;
+//Mensagem "Bilhete inválido digite novamente!"
+function showMessageError() {
+  let message = document.getElementById('successMessage');
+  message.innerHTML = `<p id="successMessage" class="error">Bilhete inválido digite novamente!</p>`
+  document.getElementById('ticketCodeField').value = ''
+}
+//Função para ativar o botão do bilhete
+function handleChange () {
+  const ticketCode = document.querySelector('#ticketCodeField').value.length;
   const ticketType = document.querySelector('.selected').innerText;
-
-  if (ticketCode && ticketType != "Escolher modalidade") {
+  if ((ticketCode >= 6) && (ticketType != "Escolher modalidade")) {
     document.querySelector('#buyTicketButton').disabled = false;
   }
   else {
     document.querySelector('#buyTicketButton').disabled = true;
   }
+} 
+// Envia para o Backend a recarga do Bilhete
+function recarregaBilhete () {
+  const cdb = document.getElementById("ticketCodeField").value;
+  const tipo = document.querySelector('.selected').innerText;
+  // cdb : codigo do bilhete do usuario
+  // tipo : tipo do bilhete escolhido 
+  let objRecarga = { cdb:cdb, tipo:tipo };
+  let url = `http://localhost:3000/Recarga/`
+
+  let res = axios.post(url, objRecarga)
+  .then(response => {
+    if (response.data) {
+      showMessageSuccess ();
+      const msg = new Comunicado (response.data.mensagem);
+      console.log(msg.get());            
+    }
+  })
+  .catch(error  =>  {
+    if (error.response) {
+      showMessageError ();
+      const msg = new Comunicado (error.response.data.mensagem);
+      console.log(msg.get());            
+    }
+  })
 }
 
-//------------------------------------------------------------------------------daqui para cima é da segunda entrega
 
-//Ativar botão "Ativar" na seção ativação e mostra a  mensagem "Recarga ativa!"
+
+
+//------------------------------------------------------------------------------daqui para cima é da segunda entrega'
+//Ativar botão "Ativar" e mostrar mensagem "Bilhete ativo"
 function Message() {
   let activateMessage = document.getElementById('successActivateMessage');
-  activateMessage.innerHTML = `<p id="successActivateMessage" class="activateTicketMessage">Recarga ativa!</p>`
+  activateMessage.innerHTML = `<p id="successActivateMessage" class="activateTicketMessage">Bilhete ativo!</p>`
 }
 
-function handleActivateChange ()
-{
+function handleActivateChange (){
   const codeVerification = document.querySelector('#ticketCodeVerification').value;
 
   if (codeVerification)
@@ -109,5 +154,13 @@ function handleActivateChange ()
   }
   else {
     document.querySelector('#activateTicketButton').disabled = true;
+  }
+}
+//Função para avisos
+function Comunicado (mensagem) {
+  this.mensagem  = mensagem;
+  this.get = function ()
+  {
+    return (this.mensagem);
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser');
 const { Console } = require('console');
-
+//-------------------------------------⬇️ Conexão com o Banco de Dados e criação das tabelas
 function banco_de_dados ()
 {
 	process.env.ORA_SDTZ = 'UTC-3'; // garante horário de Brasília
@@ -31,43 +31,54 @@ function banco_de_dados ()
 		try
 		{
 			const conexao = await this.getConexao();
-			const sql     = 'CREATE TABLE BILHETE (ID_USUARIO_BILHETE NUMBER(6)CONSTRAINT PK_ID_USUARIO_BILHETE PRIMARY KEY,'+
-			'HORA_CRIACAO NVARCHAR2(8),'+
+			const sql     = 'CREATE TABLE BILHETE (ID_USUARIO_BILHETE NUMBER(6)'+
+			'CONSTRAINT PK_ID_USUARIO_BILHETE PRIMARY KEY,'+
+			'HORA_CRIACAO VARCHAR2(8),'+
 			'DATA_CRIACAO DATE)';
 			await conexao.execute(sql);
-			console.log('Tabela criada!')
+			console.log('Tabela Bilhete!')
 		}
 		catch (erro)
-		{} // se a já existe, ignora e toca em frente
+		{} 
+	
+		try
+		{
+			const conexao = await this.getConexao();
+			const sql     = 'CREATE TABLE RECARGA(COD_RECARGA NUMBER(6, 0) CONSTRAINT RECARGA_PK PRIMARY KEY,'+
+			'TIPO VARCHAR2(20), DATA_RECARGA DATE,  HORA_RECARGA VARCHAR2(8),'+
+			'VALOR_RECARGA NUMBER(5, 2), FK_ID_BILHETE NUMBER (6, 0),'+
+			'FOREIGN KEY (FK_ID_BILHETE) REFERENCES BILHETE (ID_USUARIO_BILHETE))';
+			await conexao.execute(sql);
+			console.log('Tabela criada Recarga!')
+		}
+		catch (erro)
+		{}
 	}
 }
-
+//-------------------------------------------------------------------------⬇️ insert Bilhete
 function BILHETE (bd)
 {
 	this.bd = bd;
 	
 	this.inclua = async function (Bilhete)
 	{
-
 		const sql1 = "INSERT INTO BILHETE VALUES (:0,:1,:2)";
 		const dados = [( parseInt(Bilhete.cdb)),( Bilhete.horacri),( Bilhete.datacri)];
 	
-	
 		console.log(sql1, dados);
-		await conexao.execute(sql1, dados);//
+		await conexao.execute(sql1, dados);
 		
 		const sql2 = 'COMMIT';
 		await conexao.execute(sql2);	
 	}	
 }	
 
-
-function Bilhete (id,datac,horac)
+function Bilhete (id)
 {
-	    this.cdb = id;
-	    this.datacri = new Date().toLocaleDateString();
-	    this.horacri = new Date().toLocaleTimeString();
-}
+	this.cdb = id;
+	this.datacri = new Date().toLocaleDateString();
+	this.horacri = new Date().toLocaleTimeString();
+}	
 
 function middleWareGlobal (req, res, next)
 {
@@ -81,10 +92,9 @@ function middleWareGlobal (req, res, next)
     console.timeEnd('Requisição'); // marca o fim da requisição
 }
 
-// para a rota de CREATE
 async function inclusao (req, res)
 {
-    const Bilhetecriado = new Bilhete (req.body.id,req.body.datac,req.body.horac);
+    const Bilhetecriado = new Bilhete (req.body.id);
     try
     {
         await  global.Bilhetes.inclua(Bilhetecriado);
@@ -96,35 +106,117 @@ async function inclusao (req, res)
 		console.log(err)
     }
 }
+//-------------------------------------------------------------------------⬇️ insert Recarga
+function RECARGA (bd)
+{
+	this.bd = bd;
+	
+	this.inserir_recarga = async function (Recarga)
+	{
 
+		const sql3 = "INSERT INTO RECARGA VALUES (:0,:1,:2,:3,:4,:5)";
+		const dados2 = [(parseInt(Recarga.cdr)),(Recarga.tipo),(Recarga.datacri),
+			(Recarga.horacri),(Recarga.valor),(parseInt(Recarga.cdb))];
+	
+		console.log(sql3, dados2);
+		await conexao.execute(sql3, dados2);
+		
+		const sql4 = 'COMMIT';
+		await conexao.execute(sql4);	
+	}	
+}
+
+async function inclusaoRec (req,res)
+{
+    const Recarga_escolhida = new Recarga (req.body.tipo, req.body.cdb);
+    try
+    {	
+        await  global.Recargas.inserir_recarga(Recarga_escolhida);
+		const  sucesso = new Comunicado('Insert da Recarga concluído!');
+		return res.status(201).json(sucesso);
+	}
+	catch (err)
+	{
+		console.log('Erro no incluir da recarga');
+		console.log(err)
+		const  erro2 = new Comunicado ('O número do Bilhete digitado não é valido');
+        return res.status(409).json(erro2);
+    }
+}
+
+function createRandomNumber() {
+    const randomNumber = (Math.random() * (1000000 - 100000) + 100000).toFixed(0);
+    return randomNumber;
+}
+
+function Recarga(tipo,cdb)
+{
+	console.log(cdb);
+
+	switch(tipo) 
+	{
+		case 'Bilhete Único - R$3,10':
+			console.log('Bilhete escolhido tipo unico');
+			this.tipo = 'Unico';
+			this.valor = 3.1 ;
+		break;
+
+		case 'Bilhete Duplo - R$7,20':
+			console.log('Bilhete escolhido tipo duplo');
+			this.tipo = 'Duplo';
+			this.valor = 7.2 ;
+		break;
+
+		case 'Bilhete 7 dias - R$14,39':
+			console.log('Bilhete escolhido tipo 7dias');
+			this.tipo = '7 Dias';
+			this.valor = 14.4 ;
+		break;
+
+		case 'Bilhete 30 dias - R$35,50':
+			console.log('Bilhete escolhido tipo 30dias');
+			this.tipo = '30 Dias';
+			this.valor = 35.5;
+		break;
+	}
+
+	this.cdb = cdb;
+	this.datacri = new Date().toLocaleDateString();
+	this.horacri = new Date().toLocaleTimeString();
+	this.cdr = createRandomNumber();
+}
+
+function Comunicado (mensagem)
+{
+	this.mensagem  = mensagem;
+}
+//-------------------------------------------------------------------------------⬇️ Servidor
 async function server ()
 {
     const bd = new banco_de_dados ();
 	await bd.estrutureSe();
     global.Bilhetes = new BILHETE (bd);
+	global.Recargas = new RECARGA (bd);
 
     const express = require('express');
-    const app     = express();
-	const cors    = require('cors')
-    const bodyParser = require("body-parser");
+    const app = express();
+	const cors = require('cors')
 	const fs = require("fs");
 	const { response } = require("express");
 	const PORT = 3000;
 
-    app.use(express.json());   // faz com que o express consiga processar JSON
-	app.use(cors()); //habilitando cors na nossa aplicacao (adicionar essa lib como um middleware da nossa API - todas as requisições passarão antes por essa biblioteca).
-    app.use(middleWareGlobal); // app.use cria o middleware global
+    app.use(express.json());   
+	app.use(cors()); 
+    app.use(middleWareGlobal);
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use((req, res, next) => {
 		res.header("Access-Control-Allow-Origin", "*");
 		app.use(cors());
 		next();
 	  });
-
     app.post  ('/Bilhete'        , inclusao); 
-
-    console.log ('Servidor ativo na porta 3000')
+	app.post  ('/Recarga'        , inclusaoRec);
+    console.log ('Servidor ativo na porta 3000');
     app.listen(3000);
 }
-
 server();
